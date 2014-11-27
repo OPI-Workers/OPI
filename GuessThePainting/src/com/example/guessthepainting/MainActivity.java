c16cb7d4fe5965e357d408f51b14b5be3f3635d8
Brockhurst <nickgolkin@mail.ru>
2014-11-27 10:33:29 +0200

    gh

--- GuessThePainting/src/com/example/guessthepainting/MainActivity.java
+++ GuessThePainting/src/com/example/guessthepainting/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 
 public class MainActivity extends Activity implements OnClickListener{
 	Button start_butt;
+	Button settings_butt;
+	Button about_butt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +18,26 @@ public class MainActivity extends Activity implements OnClickListener{
         
         start_butt = (Button) findViewById(R.id.start_butt);
         start_butt.setOnClickListener(this);
+        settings_butt = (Button) findViewById(R.id.settings_butt);
+        settings_butt.setOnClickListener(this);
+        about_butt = (Button) findViewById(R.id.about_butt);
+        about_butt.setOnClickListener(this);
+        
+        
       }
 
       public void onClick(View v) {
         switch (v.getId()) {
         case R.id.start_butt:
-        	Intent intent = new Intent(this, ModePickerActivity.class);
-            startActivity(intent);
+        	Intent intent1 = new Intent(this, GameScreenActivity.class);
+            startActivity(intent1);
           break;
+        case R.id.settings_butt:
+        	intent1 = new Intent(this, SettingsActivity.class);
+            startActivity(intent1);
+        case R.id.about_butt:
+        	intent1 = new Intent(this, AboutActivity.class);
+            startActivity(intent1);
         default:
           break;
         }
